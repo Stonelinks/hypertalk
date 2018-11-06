f73a1711853de595037135e24704d2c2c4334e02
Lucas Doyle <lucas.p.doyle@gmail.com>
2018-11-05 18:57:02 -0800

    Updates

---
+++ precache-manifest.4cf3622243dfd704f0508996574c9b19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1747aff391174acaa768ffccd4c01db7",
+    "url": "./static/media/vgabios.1747aff3.bin"
+  },
+  {
+    "revision": "b72845a69cc3b056a5ba4a3d22de67ef",
+    "url": "./static/media/seabios.b72845a6.bin"
+  },
+  {
+    "revision": "186ee9ecfc1ab6480f983aa51fcbe44c",
+    "url": "./static/media/logo.186ee9ec.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2dd058c9c7d9a9d218b5",
+    "url": "./static/js/main.2dd058c9.chunk.js"
+  },
+  {
+    "revision": "e7c90d771dad049147dd",
+    "url": "./static/js/1.e7c90d77.chunk.js"
+  },
+  {
+    "revision": "2dd058c9c7d9a9d218b5",
+    "url": "./static/css/main.76ed638f.chunk.css"
+  },
+  {
+    "revision": "eefb74e13f0a4a32540e35d183e481a8",
+    "url": "./index.html"
+  }
+];
